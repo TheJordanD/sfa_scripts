4c4775da7c06d7f1004bdbf4b74795bf5db6c422
Jordan <jordandouglas@protonmail.com>
2021-02-12 16:18:31 -0600

    Reformat into endless while loop

--- src/number_guessing_game.py
+++ src/number_guessing_game.py
@@ -2,33 +2,34 @@ import random
 
 
 def generate_random_numbers():
-    return random.sample(range(1, 10), 3)
+    numbers = random.sample(range(1, 10), 3)
+    print(numbers)
+    print("I'm thinking of 3 numbers from 1 to 10. Guess one of them. ")
+
+    return numbers
 
 
-def end_game():
+def end_game(numbers):
     print("Sorry, but I was really thinking of "
-          + str(random_numbers[0])
-          + ", " + str(random_numbers[1])
-          + " and " + str(random_numbers[2]))
+          + str(numbers[0])
+          + ", " + str(numbers[1])
+          + " and " + str(numbers[2]))
     quit()
 
 
-def check_input():
+while True:
     random_numbers = generate_random_numbers()
-    print(random_numbers)
-    print("I'm thinking of 3 numbers from 1 to 10. Guess one of them. ")
     user_input = input("Your guess: ")
 
     if user_input == 'q':
         quit()
     elif not user_input.isnumeric():
-        display_wrong_answer_message()
+        print(user_input + " is not a number")
+        end_game(random_numbers)
     elif int(user_input) in random_numbers:
-        display_wrong_answer_message()
-        return True
+        print("You’re a mind reader!  My secret numbers were "
+              + str(random_numbers[0])
+              + ", " + str(random_numbers[1])
+              + " and " + str(random_numbers[2]))
     else:
-        display_wrong_answer_message()
-
-
-while check_input():
-    check_input()
+        end_game(random_numbers)
