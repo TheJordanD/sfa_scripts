9789bff0431422a309f1acbb8bda0a562bb9fdc7
Jordan <jordandouglas@protonmail.com>
2021-04-08 17:15:32 -0500

    Reformat scatter into a class with source and destination methods

--- src/scatter.py
+++ src/scatter.py
@@ -1,26 +1,49 @@
+import logging
+
 import maya.cmds as cmds
 
-selection = cmds.ls(orderedSelection=True, flatten=True)
-vertex_names = cmds.filterExpand(selectionMask=31, expand=True)
-sourceObject = selection[0]
-
-if vertex_names is None:
-    destinationObject = selection[1]
-    vertex_names = cmds.polyListComponentConversion(destinationObject, toVertex=True)
-    vertex_names = cmds.filterExpand(vertex_names, selectionMask=31)
-    print("Scatter destination: Verts on " + destinationObject)
-else:
-    print("Scatter destination: Verts")
+log = logging.getLogger(__name__)
 
 
-def perform_scatter():
-    if cmds.objectType(sourceObject) == "transform":
-        for vert in vertex_names:
-            new_instance = cmds.instance(sourceObject)
+class Scatterer(object):
+
+    def __init__(self):
+        pass
+
+    def select_source(self):
+        selection = cmds.ls(orderedSelection=True, flatten=True)
+        if len(selection) == 1 and cmds.objectType(selection) == "transform":
+            self.source_object = selection
+            log.info("Source object is now: " + str(selection))
+        else:
+            log.critical("Select a single transform object")
+
+    def select_destination(self):
+        selection = cmds.ls(orderedSelection=True, flatten=True)
+        all_verts = True
+
+        for _item in selection:
+            if ".vtx" not in _item:
+                all_verts = False
+
+        if len(selection) is 0:
+            log.critical("Select a single object or multiple vertices")
+        elif len(selection) == 1 and cmds.objectType(selection) == "transform":
+            destination_object = selection
+            log.info("Destination object is now: " + str(selection))
+            self.destination_verts = cmds.polyListComponentConversion(
+                destination_object,
+                toVertex=True)
+            self.destination_verts = cmds.filterExpand(self.destination_verts,
+                                                  selectionMask=31)
+        elif all_verts is True:
+            destination_verts = selection
+            log.info("Destination verts are now: " + str(destination_verts))
+        else:
+            log.critical("Select a single object or multiple vertices")
+
+    def perform_scatter(self):
+        for vert in self.destination_verts:
+            new_instance = cmds.instance(self.source_object)
             position = cmds.pointPosition(vert, world=True)
             cmds.xform(new_instance, translation=position)
-    else:
-        print("Please ensure the first object you select is a transfrom")
-
-
-perform_scatter()
