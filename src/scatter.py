4cf929181aa01c4a19132df1f9b1d336ca77c46c
Jordan <jordandouglas@protonmail.com>
2021-04-08 12:08:10 -0500

    Setup check for what the destination will be

--- src/scatter.py
+++ src/scatter.py
@@ -1,19 +1,28 @@
 import maya.cmds as cmds
 
 selection = cmds.ls(orderedSelection=True, flatten=True)
+vertexNames = cmds.filterExpand(selectionMask=31, expand=True,)
+sourceObject = selection[0]
 
-vertexNames = cmds.filterExpand(selectionMask=31, expand=True, )
 
-objectToInstance = selection[0]
+def check_for_verts():
+    if vertexNames == None:
+        return False
+    else:
+        return True
 
-if cmds.objectType(objectToInstance) == "transform":
 
-    for vert in vertexNames:
-        newInstance = cmds.instance(objectToInstance)
-
-        position = cmds.pointPosition(vert, world=True)
-
-        cmds.xform(newInstance, translation=position)
-
+if check_for_verts() is False:
+    print("Scatter destination: " + selection[1])
 else:
-    print("Please ensure the first object you select is a transfrom")
+    print("Scatter destination: Verts")
+
+
+
+#if cmds.objectType(sourceObject) == "transform":
+#    for vert in vertexNames:
+#        newInstance = cmds.instance(sourceObject)
+#        position = cmds.pointPosition(vert, world=True)
+#       cmds.xform(newInstance, translation=position)
+#else:
+#    print("Please ensure the first object you select is a transfrom")
