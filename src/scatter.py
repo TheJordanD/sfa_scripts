204db97081c4e78f6950bdef3ce65254b3621393
Jordan <jordandouglas@protonmail.com>
2021-04-09 09:34:55 -0500

    Add randomize rotation

--- src/scatter.py
+++ src/scatter.py
@@ -18,12 +18,26 @@ class Scatterer(object):
         self.scale_z = 1
         self.scale_z_min = 1
         self.scale_z_max = 1
-        pass
+
+        self.rot_x = 0
+        self.rot_x_min = 0
+        self.rot_x_max = 0
+        self.rot_y = 0
+        self.rot_y_min = 0
+        self.rot_y_max = 0
+        self.rot_z = 0
+        self.rot_z_min = 0
+        self.rot_z_max = 40
 
     def randomize(self):
         self.scale_x = round(rand.uniform(self.scale_x_min, self.scale_x_max), 2)
         self.scale_y = round(rand.uniform(self.scale_y_min, self.scale_y_max), 2)
         self.scale_z = round(rand.uniform(self.scale_z_min, self.scale_z_max), 2)
+
+        self.rot_x = round(rand.uniform(self.rot_x_min, self.rot_x_max), 2)
+        self.rot_y = round(rand.uniform(self.rot_y_min, self.rot_y_max), 2)
+        self.rot_z = round(rand.uniform(self.rot_z_min, self.rot_z_max), 2)
+
 
     def select_source(self):
         selection = cmds.ls(orderedSelection=True, flatten=True)
@@ -62,7 +76,9 @@ class Scatterer(object):
     def perform_scatter(self):
         for vert in self.destination_verts:
             self.randomize()
+            print(self.rot_z)
             new_instance = cmds.instance(self.source_object)
             position = cmds.pointPosition(vert, world=True)
             cmds.xform(new_instance, translation=position,
-                       scale=(self.scale_x, self.scale_y, self.scale_z))
+                       scale=(self.scale_x, self.scale_y, self.scale_z),
+                       rotation=(self.rot_x, self.rot_y, self.rot_z))
